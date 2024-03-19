e894856c353ea16f00b20ad910c3341f130d5893
danyloplaksyvy <danyloplaksyvy@gmail.com>
2024-03-19 16:08:30 +0300

    Third

--- app/src/main/java/com/example/splitpayapp/MainScreen.kt
+++ app/src/main/java/com/example/splitpayapp/MainScreen.kt
@@ -1,8 +1,24 @@
 package com.example.splitpayapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainScreen() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Test Text")
+    }
 }
