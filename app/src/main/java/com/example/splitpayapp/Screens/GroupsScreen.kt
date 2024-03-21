e12cdfb47764ced184004bdf547302971d6b27a2
danyloplaksyvy <danyloplaksyvy@gmail.com>
2024-03-21 17:53:49 +0300

    Creating Simple Interfaces

--- app/src/main/java/com/example/splitpayapp/Screens/GroupsScreen.kt
+++ app/src/main/java/com/example/splitpayapp/Screens/GroupsScreen.kt
@@ -1,2 +1,34 @@
 package com.example.splitpayapp.Screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GroupsScreen() {
+    Scaffold(topBar = {
+        TopBar(screenName = "Groups")
+    }, bottomBar = {
+        BottomAppBar {
+            IconsBottomBar(iconTypeGroups = Icons.Filled.Person)
+        }
+    }) { innerPadding ->
+        Column {
+            Text(text = "Text", modifier = Modifier.padding(innerPadding))
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GroupsScreenPreview() {
+    GroupsScreen()
+}
--- app/src/main/java/com/example/splitpayapp/Screens/RecentActivityScreen.kt
+++ app/src/main/java/com/example/splitpayapp/Screens/RecentActivityScreen.kt
@@ -16,11 +16,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,65 +36,15 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun RecentActivityScreen() {
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Spacer(modifier = Modifier.height(100.dp))
-        Text(
-            text = "Main Screen",
-            modifier = Modifier.padding(8.dp),
-            fontSize = 32.sp,
-            fontStyle = FontStyle.Italic
-        )
-        Spacer(modifier = Modifier.height(25.dp))
-        LazyColumn(
-            modifier = Modifier
-                .weight(1f)
-                .wrapContentWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(3) { index ->
-                Text(text = "Lazy Message $index", modifier = Modifier.padding(12.dp))
-            }
+    Scaffold(topBar = {
+        TopBar(screenName = "Recent Activity")
+    }, bottomBar = {
+        BottomAppBar {
+            IconsBottomBar(iconTypeGroups = Icons.Filled.Email)
         }
-        Box(
-            modifier = Modifier.run {
-                fillMaxWidth()
-                    .height(1.dp)
-                    .background(Color(87, 71, 71, 255))
-            }
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-                .padding(horizontal = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            val modifierIconButton = Modifier
-                .fillMaxHeight()
-                .weight(1f)
-                .padding(8.dp)
-
-            IconButton(onClick = { /*TODO*/ }, modifier = modifierIconButton) {
-                Icon(Icons.Default.Person, "Friends", modifier = Modifier.fillMaxSize())
-            }
-            IconButton(onClick = { /*TODO*/ }, modifier = modifierIconButton) {
-                Icon(Icons.Default.Person, "Groups", modifier = Modifier.fillMaxSize())
-            }
-            Button(onClick = { /*TODO*/ }, modifier = modifierIconButton) {
-                Icon(Icons.Default.Add, "Add Action", modifier = Modifier.fillMaxSize())
-            }
-            IconButton(onClick = { /*TODO*/ }, modifier = modifierIconButton) {
-                Icon(Icons.Default.MailOutline, "Activity", modifier = Modifier.fillMaxSize())
-            }
-            IconButton(onClick = { /*TODO*/ }, modifier = modifierIconButton) {
-                Icon(Icons.Default.Build, "Article", modifier = Modifier.fillMaxSize())
-            }
+    }) { innerPadding ->
+        Column {
+            Text(text = "Text", modifier = Modifier.padding(innerPadding))
         }
     }
 }
